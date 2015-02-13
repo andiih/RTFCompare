96d42a8038af36c68089939b22341dcc9549d094
Vaughan P Treen (RCJ) <Vaughan@RCJ>
2015-02-13 15:04:43 +0000

    2 output formats

--- RTFCompare/Program.cs
+++ RTFCompare/Program.cs
@@ -12,20 +12,44 @@ namespace RTFCompare
     {
         static void Main(string[] args)
         {
-            if (!args.Any())
+            if (args.Count()!=3)
             {
-                Console.WriteLine("Usage : RTFCompare folder");
+                Console.WriteLine("Usage : RTFCompare folder outputfile outputfile2");
                 return;
             }
             var folder = args[0];
+            var outputFile = args[1];
+            var outputFile2 = args[2];
             var tool = new Tool();
 
             var res = tool.Process(folder).FindDuplicateFiles();
 
-            foreach (var fileset in res.Select(line => string.Join(",", line)))
+            var lines = res.Select(line => string.Join(",", line));
+            using (System.IO.StreamWriter ofile = new System.IO.StreamWriter(outputFile))
             {
-                Console.WriteLine(fileset);
+                foreach (string line in lines)
+                {
+                        ofile.WriteLine(line);
+                        Console.Write("+");
+                }
             }
+
+
+            using (System.IO.StreamWriter ofile = new System.IO.StreamWriter(outputFile2))
+            {
+                foreach (var dupelist in res)
+                {
+                    foreach (var line in dupelist.Skip(1))
+                    ofile.WriteLine(line);
+                    Console.Write("*");
+                }
+            }
+
+//            foreach (var fileset in res.Select(line => string.Join(",", line)))
+//            {
+//                Console.WriteLine(fileset);
+//            }
+            Console.WriteLine("Done");
             Console.ReadLine();
         }
     }
